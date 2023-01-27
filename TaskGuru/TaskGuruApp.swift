e9add9aaf6408195b0e4bea3c4f30afd6c312576
Joe Pham <rungxanh2901@gmail.com>
2023-01-27 18:18:39 -0500

    Initial Commit

---
+++ TaskGuru/TaskGuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskGuruApp.swift
+//  TaskGuru
+//
+//  Created by Joe Pham on 2023-01-27.
+//
+
+import SwiftUI
+
+@main
+struct TaskGuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
