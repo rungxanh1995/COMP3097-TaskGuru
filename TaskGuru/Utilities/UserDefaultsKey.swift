8fe65b832cd1dcd81739353681bb2340839c6772
Joe Pham <rungxanh2901@gmail.com>
2023-02-13 16:55:39 -0500

    add user defaults key enum

---
+++ TaskGuru/Utilities/UserDefaultsKey.swift
@@ -0,0 +1,16 @@
+//
+//  UserDefaultsKey.swift
+//  TaskGuru
+//
+//  Created by Joe Pham on 2023-02-13.
+//
+
+import Foundation
+
+enum UserDefaultsKey {
+	static let hapticsReduced: String = "hapticsReduced"
+	static let systemTheme: String = "systemTheme"
+	static let isOnboarding: String = "isOnboarding"
+	static let isShowingTabBadge: String = "isShowingTabBadge"
+}
+
