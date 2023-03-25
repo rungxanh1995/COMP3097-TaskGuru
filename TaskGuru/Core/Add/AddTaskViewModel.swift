1748a94559e8d23f8af28533f3ffc018af57bd04
Rauf <rauf.code@gmail.com>
2023-03-25 13:26:38 -0400

    finish Add task screen + view model

--- TaskGuru/Core/Add/AddTaskViewModel.swift
+++ TaskGuru/Core/Add/AddTaskViewModel.swift
@@ -10,10 +10,26 @@ import Foundation
 
 extension AddTaskScreen {
 	final class ViewModel: ObservableObject {
+		@Published var taskName: String = ""
+		@Published var dueDate: Date = .now
+		@Published var taskType: TaskType = .personal
+		@Published var taskStatus: TaskStatus = .new
+		@Published var taskNotes: String = ""
+
 		private let listViewModel: HomeViewModel
 				
 		init(parentVM: HomeViewModel) {
 			listViewModel = parentVM
+		}
+
+		func addTask(name: inout String, dueDate: Date, type: TaskType,
+					 status: TaskStatus, notes: String) {
+			assignDefaultTaskName(to: &name)
+			listViewModel.addTask(name: &name, type: type, dueDate: dueDate, status: status, notes: notes)
+		}
+
+		fileprivate func assignDefaultTaskName(to name: inout String) {
+			if name == "" { name = "Untitled Task" }
 		}
 	}
 }
