cdc37e53943bda8f0b9188b03a7cebfc2cf8ec4a
Marco Stevanella <36211247+floydcoder@users.noreply.github.com>
2023-04-12 18:34:30 -0400

    Complete Home screen module (#38)
    * finish home screen module for final implementation
    
    * Fix crash in StorageProviderImpl with mixmatched sort key
    
    * Update top header comments
    
    ---------
    
    Co-authored-by: Joe Pham <rungxanh2901@gmail.com>

--- TaskGuru/Core/Home/HomeScreen.swift
+++ TaskGuru/Core/Home/HomeScreen.swift
@@ -2,14 +2,14 @@
 //  HomeScreen.swift
 //  TaskGuru
 //
-//  Created by Marco Stevanella on 2023-02-17.
+//  Created by Marco Stevanella on 2023-04-12.
 //  Student ID: 101307949
 //
 
 import SwiftUI
 
 struct HomeScreen: View {
-	@EnvironmentObject private var vm: HomeViewModel
+	@EnvironmentObject var vm: HomeViewModel
 	@StateObject private var tabState: AppState = .init()
 	@State private var selectedTask: TaskItem?
 
@@ -19,11 +19,13 @@ struct HomeScreen: View {
 	@Preference(\.isTodayDuesHighlighted) private var duesHighlighted
 	@Preference(\.isPreviewEnabled) private var isPreviewEnabled
 	@Preference(\.contextPreviewType) private var previewType
-	
+
 	var body: some View {
 		NavigationStack(path: $tabState.navPath) {
 			ZStack {
-				if vm.noPendingTasksLeft {
+				if vm.isFetchingData {
+					ProgressView { Text("pending.info.fetchingData") }
+				} else if vm.allTasks.isEmpty {
 					emptyTaskText.padding()
 				} else {
 					List {
@@ -35,8 +37,10 @@ struct HomeScreen: View {
 			}
 			.listStyle(.plain)
 			.playConfetti($confettiCounter)
+			.onAppear(perform: vm.fetchTasks)
+			.onChange(of: selectedTask) { _ in vm.fetchTasks() }
 			.navigationDestination(for: TaskItem.self) { task in
-				DetailScreen(vm: .init(for: task, parentVM: vm))
+				DetailScreen(vm: .init(for: task))
 			}
 			.navigationBarTitleDisplayMode(.inline)
 			.toolbar {
@@ -58,7 +62,20 @@ struct HomeScreen: View {
 				AddTaskScreen(vm: .init(parentVM: self.vm))
 			}
 			.fullScreenCover(item: $selectedTask) { task in
-				DetailScreen(vm: .init(for: task, parentVM: vm))
+				DetailScreen.EditMode(vm: .init(for: task))
+			}
+			.confirmationDialog(
+				"home.clearDoneTasks.alert",
+				isPresented: $vm.isConfirmingClearDoneTasks,
+				titleVisibility: .visible
+			) {
+				Button("contextMenu.clearDoneTasks", role: .destructive) {
+					vm.clearDoneTasks()
+					haptic(.notification(.success))
+				}
+				Button("contextMenu.cancel", role: .cancel) {
+					haptic(.buttonPress)
+				}
 			}
 		}
 		.environmentObject(tabState)
@@ -78,61 +95,60 @@ extension HomeScreen {
 		.onTapGesture { vm.isShowingAddTaskView.toggle() }
 	}
 
-	private var emptyFilteredListText: some View {
-		Text("home.info.sectionEmpty")
+	@ViewBuilder private var emptyFilteredListText: some View {
+		let emptyListSentence = LocalizedStringKey("home.info.sectionEmpty")
+		Text(emptyListSentence)
 			.font(.callout)
 			.foregroundColor(.secondary)
 	}
 
-    private var overdueSection: some View {
-        Section {
-            let overdues = vm.searchResults.filter { $0.dueDate.isPastToday }
+	@ViewBuilder private var overdueSection: some View {
+		Section {
+			let overdues = vm.searchResults.filter { $0.dueDate.isPastToday }
 
-            if overdues.isEmpty {
-                emptyFilteredListText
-            } else {
-                filteredList(of: overdues)
-            }
-        } header: {
-            Text("home.sections.overdue").bold()
-                .foregroundColor(.red)
-        }
-    }
+			if overdues.isEmpty {
+				emptyFilteredListText
+			} else {
+				filteredList(of: overdues)
+			}
+		} header: {
+			Text("home.sections.overdue").bold().foregroundColor(.appPink)
+		}
+	}
 
-    private var dueTodaySection: some View {
-        Section {
-            let dues = vm.searchResults.filter { $0.dueDate.isWithinToday }
+	@ViewBuilder private var dueTodaySection: some View {
+		Section {
+			let dues = vm.searchResults.filter { $0.dueDate.isWithinToday }
 
-            if dues.isEmpty {
-                emptyFilteredListText
-            } else {
-                filteredList(of: dues)
-                    .if(duesHighlighted) { list in
-                        list
-                            .listRowBackground(DynamicHighlightBackground())
-                    }
-            }
-        } header: {
-            Text("home.sections.dueToday").bold()
-                .foregroundColor(.yellow)
-        }
-    }
+			if dues.isEmpty {
+				emptyFilteredListText
+			} else {
+				filteredList(of: dues)
+					.if(duesHighlighted) { list in
+						list
+							.listRowBackground(DynamicHighlightBackground())
+					}
+			}
+		} header: {
+			Text("home.sections.dueToday").bold().foregroundColor(.appYellow)
+		}
+	}
 
-	private var upcomingSection: some View {
+	@ViewBuilder private var upcomingSection: some View {
 		Section {
 			let upcomings = vm.searchResults.filter { $0.dueDate.isFromTomorrow }
-			
+
 			if upcomings.isEmpty {
 				emptyFilteredListText
 			} else {
 				filteredList(of: upcomings)
 			}
 		} header: {
-			Text("home.sections.upcoming").bold()
-                .foregroundColor(.teal)
+			Text("home.sections.upcoming").bold().foregroundColor(.appClover)
 		}
 	}
 
+	@ViewBuilder
 	private func filteredList(of tasks: [TaskItem]) -> some View {
 		ForEach(tasks) { task in
 			NavigationLink(value: task) {
@@ -143,59 +159,72 @@ extension HomeScreen {
 					cell.contextMenu { makeContextMenu(for: task) }
 				} elseCase: { cell in
 					cell.contextMenu { makeContextMenu(for: task) } preview: {
-						DetailScreen(vm: .init(for: task, parentVM: vm))
+						DetailScreen(vm: .init(for: task))
 					}
 				}
 			}
 			.swipeActions(edge: .leading) {
 				switch task.status {
 				case .new:
-					markInProgressButton(for: task).tint(.yellow)
+					markInProgressButton(for: task).tint(.appYellow)
 				case .inProgress:
-					markNewButton(for: task).tint(.teal)
+					markNewButton(for: task).tint(.appClover)
 				case .done:
-					markNewButton(for: task).tint(.teal)
-					markInProgressButton(for: task).tint(.yellow)
+					markNewButton(for: task).tint(.appClover)
+					markInProgressButton(for: task).tint(.appYellow)
 				}
 			}
 			.swipeActions(edge: .trailing, allowsFullSwipe: true) {
-				deleteButton(for: task).tint(.pink)
+				deleteButton(for: task).tint(.appPink)
 				if task.isNotDone {
-					markDoneButton(for: task).tint(.indigo)
+					markDoneButton(for: task).tint(.appIndigo)
 				}
 			}
 		}
 	}
-	
+
 	@ViewBuilder
 	private func makeContextMenu(for task: TaskItem) -> some View {
-		if task.isNotDone {
-			Button {
-				withAnimation { vm.markAsDone(task) }
-				if isConfettiEnabled { confettiCounter += 1 }
-			} label: {
-				Label { Text("Mark as Done") } icon: { SFSymbols.checkmark }
-			}
-		}
-		Button { selectedTask = task } label: {
-			Label { Text("Edit") } icon: { SFSymbols.pencilSquare }
-		}
-		Divider()
-		
-		Menu {
-			Button(role: .cancel) {} label: {
-				Label { Text("Cancel") } icon: { SFSymbols.xmark }
-			}
-			Button(role: .destructive) {
-				withAnimation { vm.delete(task) }
-			} label: {
-				Label { Text("Delete") } icon: { SFSymbols.trash }
-			}
+		markAsButtons(for: task)
+		Button {
+			selectedTask = task
+			haptic(.buttonPress)
 		} label: {
-			Label { Text("Delete") } icon: { SFSymbols.trash }
+			Label { Text("contextMenu.task.edit") } icon: { SFSymbols.pencilSquare }
+		}
+
+		Menu {
+			Button(role: .cancel) {
+				haptic(.buttonPress)
+			} label: {
+				Label { Text("contextMenu.cancel") } icon: { SFSymbols.xmark }
+			}
+			deleteButton(for: task)
+		} label: {
+			Label { Text("contextMenu.task.delete") } icon: { SFSymbols.trash }
 		}
 	}
-	
+
+	@ViewBuilder private func markAsButtons(for task: TaskItem) -> some View {
+		switch task.status {
+		case .new:
+			Section {
+				markInProgressButton(for: task)
+				markDoneButton(for: task)
+			} header: { Text("contextMenu.task.markAs") }
+		case .inProgress:
+			Section {
+				markNewButton(for: task)
+				markDoneButton(for: task)
+			} header: { Text("contextMenu.task.markAs") }
+		case .done:
+			Section {
+				markNewButton(for: task)
+				markInProgressButton(for: task)
+			} header: { Text("contextMenu.task.markAs") }
+		}
+	}
+
 	private func markNewButton(for task: TaskItem) -> some View {
 		Button {
 			withAnimation { vm.markAsNew(task) }
@@ -206,7 +235,7 @@ extension HomeScreen {
 			} icon: { SFSymbols.sparkles }
 		}
 	}
-	
+
 	private func markInProgressButton(for task: TaskItem) -> some View {
 		Button {
 			withAnimation { vm.markAsInProgress(task) }
@@ -217,7 +246,7 @@ extension HomeScreen {
 			} icon: { SFSymbols.circleArrows }
 		}
 	}
-	
+
 	private func markDoneButton(for task: TaskItem) -> some View {
 		Button {
 			withAnimation { vm.markAsDone(task) }
@@ -229,7 +258,7 @@ extension HomeScreen {
 			} icon: { SFSymbols.checkmark }
 		}
 	}
-	
+
 	private func deleteButton(for task: TaskItem) -> some View {
 		Button(role: .destructive) {
 			withAnimation { vm.delete(task) }
@@ -240,7 +269,7 @@ extension HomeScreen {
 			} icon: { SFSymbols.trash }
 		}
 	}
-	
+
 	private var todaysDate: some View {
 		Label {
 			Text(Date().formatted(.dateTime.weekday().day()))
@@ -249,14 +278,15 @@ extension HomeScreen {
 			SFSymbols.calendarWithClock
 		}
 		.labelStyle(.titleAndIcon)
-		.foregroundColor(.yellow)
+		.foregroundColor(.appYellow)
 	}
-	
+
 	private var addTaskButton: some View {
 		Button {
+			haptic(.buttonPress)
 			vm.isShowingAddTaskView.toggle()
 		} label: {
-			Label { Text("Add Task") } icon: { SFSymbols.plus }
+			Label { Text("label.task.add") } icon: { SFSymbols.plus }
 		}
 		.buttonStyle(.bordered)
 		.buttonBorderShape(.capsule)
